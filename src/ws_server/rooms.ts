d857936202e53f6c0a99b1078efff8ea71fc9a6d
andrewk0704 <andrew.kozhenov@mail.ru>
2025-05-20 01:57:17 +0400

    feat: Start game

--- src/ws_server/rooms.ts
+++ src/ws_server/rooms.ts
@@ -3,8 +3,9 @@ import { wsServer, WsSendCommands } from "./index";
 import { Room, AddUserToRoomClientResponseData} from "./model";
 import { v4 as uuidv4 } from 'uuid';
 import {userDb} from './registration';
+import { createGame } from "./game";
 
-let roomsDb:Room[]=[];
+export let roomsDb:Room[]=[];
 
 export const castToAllClients = (type: WsSendCommands, data: string) => {
     let result:any='';
@@ -60,8 +61,9 @@ export const addUserToRoom = (ws: WebSocket, data: string) => {
           for(let j=0; j<roomsDb.length; j++){
             if(roomsDb[j]?.roomId.toString()===indexRoom.toString() && roomsDb[j]?.roomUsers[0]?.name!==userDb[i].name){
               roomsDb[j]?.roomUsers.push({name:userDb[i].name,index:userDb[i].index});
-              console.log(roomsDb[0]?.roomUsers);
               updateRoom();
+              createGame(roomsDb[j]?.roomUsers);
+              console.log(roomsDb[j]?.roomUsers)
             }
           }
         }
